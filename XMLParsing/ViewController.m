acb9a01651e9d7d986c30e378fe62fb4cdc3ae84
Christopher Constable <chris.m.constable@gmail.com>
2013-10-04 11:51:28 -0400

    Added table view methods.

--- XMLParsing/ViewController.m
+++ XMLParsing/ViewController.m
@@ -27,6 +27,59 @@
 - (void)requestXMLData
 {
     // XML Data: http://www.mobiquityinc.com/news.rss
+    
+    // Try it out using different parsers
+    // [self parseXMLUsingGData]; etc...
+}
+
+- (void)parseXMLUsingGData
+{
+    
+}
+
+- (void)parseXMLUsingTouchXML
+{
+    
+}
+
+- (void)parseXMLUsingRaptureXML
+{
+    
+}
+
+- (void)parseXMLUsingNSXMLParser
+{
+    
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:@"Cell"];
+    }
+    
+    // TODO: Setup cell
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
